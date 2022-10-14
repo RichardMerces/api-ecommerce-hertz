d4e033f2b58b1ed8d0c07e640adf1e80112493ec
unknown <thayjsilva@gmail.com>
2022-10-14 11:36:18 -0300

    atualizacaoDoArquivo.Module

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { ProdutosController } from './produtos.controller';
 import { produtosProviders } from './produtos.providers';
 import { ProdutosService } from './produtos.service';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [ProdutosController],
   providers: [
     ...produtosProviders,
     ProdutosService,
