e8ed083d7bf9af3d34dc49b110b82bee5739af3d
francieleber <francielebernardino545@gmail.com>
2022-10-14 11:39:31 -0300

    tabela cliente

--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { ClienteController } from './cliente.controller';
 import { clienteProviders } from './cliente.providers';
 import { ClienteService } from './cliente.service';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [ClienteController],
   providers: [
     ...clienteProviders,
     ClienteService,
