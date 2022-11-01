9def76b52c9075ebac9d71b094f67b74d3c095ec
RichardMerces <richardmerces@outlook.com>
2022-10-31 21:20:45 -0300

    comitando para autenticado

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { UsuarioModule } from './usuario/usuario.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '34Uffd%%sj2344',
+      password: 'root',
       database: 'db_ecommerce',
       entities: [Produto, Categoria, Usuario],
         synchronize: true,
